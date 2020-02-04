bbadaf28e48224c710e0e736726cd308ec7da07d
丘天惠 <43975076+Autumnhui@users.noreply.github.com>
2020-02-04 20:58:48 +0800

    细分数据，增加各国、地区列表以及对应数目。

--- data.py
+++ data.py
@@ -15,29 +15,53 @@ heal_total_china = list()
 
 # 全国确诊人数
 confirm_total_china.append(int(data['chinaTotal']['confirm']))
-# print('全国确诊人数：',confirm_total_china)
+print('全国确诊人数：',confirm_total_china)
 # 全国疑似人数
 suspect_total_china.append(int(data['chinaTotal']['suspect']))
-# print('全国疑似人数：',suspect_total_china)
+print('全国疑似人数：',suspect_total_china)
 # 全国死亡人数
 dead_total_china.append(int(data['chinaTotal']['dead']))
-# print('全国死亡人数：',dead_total_china)
+print('全国死亡人数：',dead_total_china)
 # 全国治愈人数
 heal_total_china.append(int(data['chinaTotal']['heal']))
-# print('全国治愈人数：',heal_total_china)
+print('全国治愈人数：',heal_total_china)
 # 数据更新时间
 time= data['lastUpdateTime']
-# print('数据更新时间：',time)
+print('数据更新时间：',time)
 
 # 整体数据
 world_data= data['areaTree']
-print(world_data)
+# print(world_data)
 
-# 确诊国家列表
-country_list =list()
+# 有病例国家列表
+country_list =[]
 for i in world_data:
     country_list.append(i['name'])
-print(country_list)
+print('有病例国家列表',country_list)
+
+# 各国家数目
+country_num_list =[]
+for i in world_data:
+    country_num_list.append(i['total']['confirm'])
+print('各国家数目',country_num_list)
+
+# 中国地区总列表
+china_list = [world_data[0]['children']]
+print('中国地区总列表',china_list)
+
+# 中国各省份列表
+china_province_list = []
+for i in china_list[0]:
+    china_province_list.append(i['name'])
+print('中国各省份列表',china_province_list)
+
+# 中国各省份病例数目列表
+china_province_num_list = []
+for i in china_list[0]:
+    china_province_num_list.append(i['total']['confirm'])
+print('中国各省份病例数目列表',china_province_num_list)
+
+
 
 
 # for i in len(data['areaTree']):
